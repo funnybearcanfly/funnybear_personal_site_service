793fb7b45b6e1a9b9ea36f6f1cb0916ff901de89
Yixiong <xiongyenihao@hotmail.com>
2017-08-23 22:30:15 +0800

    update post table structure

--- src/main/java/com/funnybear/siteService/model/Post.java
+++ src/main/java/com/funnybear/siteService/model/Post.java
@@ -13,16 +13,22 @@ import javax.persistence.Table;
 @Table(name = "posts")
 public class Post {
 	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	@Column(name="authur_id")
-	private int authurId;
-	@Column(name="last_modified_time")
+	@Column(name = "authur_name")
+	private String authurName;
+	@Column(name = "last_modified_time")
 	private Date lastModifiedTime;
-	@Column(name="title")
+	@Column(name = "title")
 	private String title;
-	@Column(name="content")
+	@Column(name = "tag")
+	private String tag;
+	@Column(name = "category")
+	private String category;
+	@Column(name = "description")
+	private String description;
+	@Column(name = "content")
 	private String content;
 
 	public int getId() {
@@ -33,20 +39,20 @@ public class Post {
 		this.id = id;
 	}
 
-	public int getAuthurId() {
-		return authurId;
+	public String getAuthurName() {
+		return authurName;
 	}
 
-	public void setAuthurId(int authurId) {
-		this.authurId = authurId;
+	public void setAuthurName(String authurName) {
+		this.authurName = authurName;
 	}
 
-	public Date getLastUpdatedTime() {
+	public Date getLastModifiedTime() {
 		return lastModifiedTime;
 	}
 
-	public void setLastUpdatedTime(Date lastUpdatedTime) {
-		this.lastModifiedTime = lastUpdatedTime;
+	public void setLastModifiedTime(Date lastModifiedTime) {
+		this.lastModifiedTime = lastModifiedTime;
 	}
 
 	public String getTitle() {
@@ -55,6 +61,30 @@ public class Post {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getContent() {
